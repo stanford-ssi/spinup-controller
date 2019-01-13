943d9798c30f37a2502a8c7b25ac6cf04cb2ef27
Chandler Watson <watsonc@stanford.edu>
2019-01-13 13:25:21 -0800

    Get outline of base code finished

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,23 @@
  */
 #include "Arduino.h"
 #include "min.h"
+#define S6C     Serial1
+#define ODRIVE  Serial2
 
 struct min_context min_ctx;
-uint32_t last_sent;
+uint8_t cur_min_id; 
+
+void min_tx_start(uint8_t port) {}
+void min_tx_finished(uint8_t port) {}
 
 uint16_t min_tx_space(uint8_t port)
 {
-  return ODrive.availableForWrite();
+  return ODRIVE.availableForWrite();
 }
 
 void min_tx_byte(uint8_t port, uint8_t byte)
 {
-  ODrive.write(&byte, 1U);  
+  ODRIVE.write(&byte, 1U);  
 }
 
 uint32_t min_time_ms(void)
@@ -22,21 +27,16 @@ uint32_t min_time_ms(void)
   return millis();
 }
 
-void min_application_handler(uint8_t min_id, uint8_t *min_payload, uint8_t len_payload, uint8_t port)
+void min_application_handler(uint8_t min_id, uint8_t *min_payload, 
+    uint8_t len_payload, uint8_t port)
 {
-  Serial.print("MIN frame with ID ");
-  Serial.print(min_id);
-  Serial.print(" received at ");
-  Serial.println(millis());
-  min_id++;
-  // The frame echoed back doesn't go through the transport protocol: it's send back directly
-  min_send_frame(&min_ctx, min_id, min_payload, len_payload);
+  ODRIVE.write(min_payload, len_payload);
+  cur_min_id = min_id + 1;
 }
 
 void setup()
 {
   min_init_context(&min_ctx, 0);
-  last_sent = millis();
 }
 
 void loop()
@@ -44,8 +44,8 @@ void loop()
   char buf[32];
   size_t buf_len;
 
-  if(SerialUSB.available() > 0) {
-    buf_len = SerialUSB.readBytes(buf, 32U);
+  if(S6C.available() > 0) {
+    buf_len = S6C.readBytes(buf, 32U);
   }
   else {
     buf_len = 0;
@@ -53,10 +53,8 @@ void loop()
 
   min_poll(&min_ctx, (uint8_t *)buf, (uint8_t)buf_len);
 
-  if(!min_queue_frame(&min_ctx, 0x33U, (uint8_t *)&now, 4U)) {
-    // The queue has overflowed for some reason
-    Serial.print("Can't queue at time ");
-    Serial.println(millis());
+  if (ODRIVE.available() > 0) {
+    buf_len = ODRIVE.readBytes(buf, 32U);
+    min_send_frame(&min_ctx, cur_min_id++, (uint8_t *)buf, (uint8_t)buf_len);
   }
-  last_sent = now;
 }
