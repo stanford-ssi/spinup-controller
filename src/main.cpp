42efef60aed8125084f8026266987c79be44905c
Chandler Watson <watsonc@stanford.edu>
2019-01-13 12:35:41 -0800

    Scaffold off the example MIN code

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,62 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
+/*
+ * main.cpp: simply forwards MIN to UART
  */
 #include "Arduino.h"
 #include "min.h"
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 13
-#endif
+struct min_context min_ctx;
+uint32_t last_sent;
+
+uint16_t min_tx_space(uint8_t port)
+{
+  return ODrive.availableForWrite();
+}
+
+void min_tx_byte(uint8_t port, uint8_t byte)
+{
+  ODrive.write(&byte, 1U);  
+}
+
+uint32_t min_time_ms(void)
+{
+  return millis();
+}
+
+void min_application_handler(uint8_t min_id, uint8_t *min_payload, uint8_t len_payload, uint8_t port)
+{
+  Serial.print("MIN frame with ID ");
+  Serial.print(min_id);
+  Serial.print(" received at ");
+  Serial.println(millis());
+  min_id++;
+  // The frame echoed back doesn't go through the transport protocol: it's send back directly
+  min_send_frame(&min_ctx, min_id, min_payload, len_payload);
+}
 
 void setup()
 {
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  min_init_context(&min_ctx, 0);
+  last_sent = millis();
 }
 
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
+  char buf[32];
+  size_t buf_len;
 
-  // wait for a second
-  delay(1000);
+  if(SerialUSB.available() > 0) {
+    buf_len = SerialUSB.readBytes(buf, 32U);
+  }
+  else {
+    buf_len = 0;
+  }
 
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
+  min_poll(&min_ctx, (uint8_t *)buf, (uint8_t)buf_len);
 
-   // wait for a second
-  delay(1000);
+  if(!min_queue_frame(&min_ctx, 0x33U, (uint8_t *)&now, 4U)) {
+    // The queue has overflowed for some reason
+    Serial.print("Can't queue at time ");
+    Serial.println(millis());
+  }
+  last_sent = now;
 }
